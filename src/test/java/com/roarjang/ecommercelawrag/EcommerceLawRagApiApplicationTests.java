95be374cf05d8d6ccdb3e8608b0e0f94991391d2
roarjang <roarjang@gmail.com>
2026-07-14 19:37:38 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/com/roarjang/ecommercelawrag/EcommerceLawRagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roarjang.ecommercelawrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLawRagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
